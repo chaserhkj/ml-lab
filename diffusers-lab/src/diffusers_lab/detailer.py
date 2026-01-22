eeec950bcd43e2e9460744f6cd276688d68f931b
Chaser Huang <huangkangjing@gmail.com>
2026-01-22 02:48:28 -0500

    optimize detailer cropping

--- diffusers-lab/src/diffusers_lab/detailer.py
+++ diffusers-lab/src/diffusers_lab/detailer.py
@@ -7,7 +7,7 @@ from transformers import Sam3Processor, Sam3Model, logging
 from PIL import Image, ImageFilter
 from typing import Any
 
-from . import scale_largest_dimension_to, preview_image
+from . import scale_largest_dimension_to, preview_image, shrink_bbox_to_div
 from .conditioning import get_sdxl_prompt_embeds
 # pyright: reportUnknownMemberType=none, reportAny=none, reportExplicitAny=none
 # pyright: reportUnknownArgumentType=none, reportUnknownVariableType=none
@@ -33,6 +33,7 @@ class SDXLDetailer(object):
                 strength: float = 0.4,
                 additional_diffusion_params: dict[str, Any] | None = None,
                 preview_mask: bool = False,
+                crop_64_div: bool = True,
                 soft_blend_radius: int = 3) -> Image.Image:
         model = Sam3Model.from_pretrained("facebook/sam3")
         model = model.to("cuda") # pyright: ignore[reportArgumentType]
@@ -68,6 +69,7 @@ class SDXLDetailer(object):
                 strength = strength,
                 additional_diffusion_params = additional_diffusion_params,
                 preview_mask = preview_mask,
+                crop_64_div = crop_64_div,
                 soft_blend_radius = soft_blend_radius,
             )
             detailed.paste(detailed_patch, (int(box[0]), int(box[1])), blend_mask)
@@ -84,30 +86,51 @@ class SDXLDetailer(object):
             strength: float = 0.4,
             additional_diffusion_params: dict[str, Any] | None = None,
             preview_mask: bool = False,
+            crop_64_div: bool = True,
             soft_blend_radius: int = 3) -> tuple[Image.Image, Image.Image, tuple[int, int, int, int]]:
         boxes = masks_to_boxes(mask.unsqueeze(0))
         mask_image = Image.fromarray(mask.numpy().astype(np.uint8)*255, mode="L")
         if preview_mask:
-            preview_mask(mask_image)
+            preview_image(mask_image)
         box = boxes[0].cpu().numpy()
         print(f"Detailer: Processing mask with bbox {box.tolist()}")
         growth_w = int((box[2] - box[0]) * context_ratio)
         growth_h = int((box[3] - box[1]) * context_ratio)
         box = box + [-growth_w, -growth_h, growth_w, growth_h]
         box = np.clip(box, 0, [image.size[0], image.size[1], image.size[0], image.size[1]])
-        box = box.astype(np.uint32)
-        print(f"Detailer: Expanded context bbox to {box.tolist()}")
+        box = box.astype(np.uint32).tolist()
+        print(f"Detailer: Expanded context bbox to {box}")
         cropped = image.crop(tuple(box))
         cropped_mask = mask_image.crop(tuple(box))
 
         inpaint_pipe = StableDiffusionXLInpaintPipeline.from_pipe(self._pipeline)
         assert isinstance(inpaint_pipe, StableDiffusionXLInpaintPipeline)
-        upscaled = scale_largest_dimension_to(cropped)
-        upscaled_mask = scale_largest_dimension_to(cropped_mask)
-        print(f"Detailer: Upscaled diffusion dimension: {upscaled.size}")
+        upscaled, upscale = scale_largest_dimension_to(cropped)
+        upscaled_mask, _ = scale_largest_dimension_to(cropped_mask)
+        print(f"Detailer: Upscaled dimension: {upscaled.size}")
+        upscale_crop_box = (0, 0, upscaled.size[0], upscaled.size[1])
+        if crop_64_div:
+            upscale_crop_box = shrink_bbox_to_div(upscale_crop_box)
+        print(f"Detailer: 64-div Crop: {upscale_crop_box}, size: ")
+        patch = upscaled.crop(upscale_crop_box)
+        patch_mask = upscaled_mask.crop(upscale_crop_box)
+        print(f"Detailer: Diffusion dimensions: {patch.size}")
+
+        downscale_size = (int(patch.size[0] / upscale), int(patch.size[1]/upscale))
+        patch_origin = (
+            box[0] + int(upscale_crop_box[0]/upscale),
+            box[1] + int(upscale_crop_box[1]/upscale)
+        )
+        patch_bbox = (
+            patch_origin[0], patch_origin[1],
+            patch_origin[0] + downscale_size[0],
+            patch_origin[1] + downscale_size[1]
+        )
+        print(f"Detailer: Final patch bbox: {patch_bbox}")
+
         params: dict[str, Any] = dict(
-            image = upscaled,
-            mask_image = upscaled_mask,
+            image = patch,
+            mask_image = patch_mask,
             num_inference_steps = num_inference_steps,
             guidance_scale = guidance_scale,
             strength = strength,
@@ -120,7 +143,9 @@ class SDXLDetailer(object):
             params["generator"] = generator
         detailed_patch = inpaint_pipe(**params)[0][0] # pyright: ignore[reportIndexIssue]
         assert isinstance(detailed_patch, Image.Image) 
-        detailed_final = detailed_patch.resize(cropped.size, Image.Resampling.LANCZOS)
-        blend_mask = cropped_mask.filter(ImageFilter.GaussianBlur(radius=soft_blend_radius))
-        print(f"Detailer: Finished mask with bbox {box.tolist()}")
-        return (detailed_final, blend_mask, tuple(box))
+
+        detailed_final = detailed_patch.resize(downscale_size, Image.Resampling.LANCZOS)
+        final_mask = patch_mask.resize(downscale_size, Image.Resampling.LANCZOS)
+        blend_mask = final_mask.filter(ImageFilter.GaussianBlur(radius=soft_blend_radius))
+        print(f"Detailer: Finished mask with bbox {box}")
+        return (detailed_final, blend_mask, patch_bbox)
